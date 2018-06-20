f51acc46cdf7281be2abf21cf08bddde1719bad9
ylatorre <yvanlatorre@hotmail.fr>
2018-06-20 12:03:14 +0200

    poulet

--- src/components-modules/MenuItems.js
+++ src/components-modules/MenuItems.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Itemblock from './Itemblock'
 
+
 const MenuItems = ({ items = [] }) =>
   <div className="s-mt-1 s-d-flex">
     {items.map((item, index) =>
-      <Itemblock key={index} src="http://via.placeholder.com/150x150">
+      <Itemblock key={index} src="src/icon/Logo_Poulet.png">
         Name-{index+1} item Name-{index+1} item Name-{index+1} item PRIX
       </Itemblock>)}
   </div>
