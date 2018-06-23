5d2efa7821c0a2a84b0aaa45b6116c0c45a02c16
shvan7 <shvann.7@gmail.com>
2018-06-23 20:44:57 +0200

    page spots and block stage

--- src/components-modules/Itemblock.js
+++ src/components-modules/Itemblock.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Itemblock.css'
 
-const Itemblock = ({ src, children }) =>
+import img1 from '../img/LogoPoulet.png'
+
+const Itemblock = ({ children }) =>
   <div className="s-thumbnail">
-    <img className="s-img-thumbnail" src={src} alt="item image" style={{ height: "28vw"}}/>
+    <img className="s-img-thumbnail" src={img1} alt="item image"/>
     <p className="s-detail-thumbnail">{children}</p>
   </div>
 
